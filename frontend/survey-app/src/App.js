d7bed4cebdefd2d8fa37b6a321f4b2bafa611668
Elzbieta <elzbieta.glazar@gmail.com>
2020-02-26 00:15:35 +0100

    Migrating from Figma to React

--- frontend/survey-app/src/App.js
+++ frontend/survey-app/src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 
 //components
-import Header from './components/headerComponent/header';
-import Footer from './components/footerComponent/footer';
-import Homepage from './components/pages/homePage';
+//import Header from './components/headerComponent/header';
+//import Footer from './components/footerComponent/footer';
+//import Homepage from './components/pages/homePage';
 
 //includes
-import './Assets/css/default.min.css';
-
+//import './Assets/css/default.min.css';
+import './Assets/css/surveyapp.css';
 function App() {
   return (
-    <div className="App">
-
-      <Header />
+    <div id="landing-page-background">
+      <p id="landing-page-text">Get The Feedback You Need</p>
+      <p id="landing-page-logo">SurveyApp</p>
+      <a id="button-login" href="#">LOG IN</a>
+      <a id="button-sign-up" href="#">SIGN UP</a>
+      <a id="button-about" href="#">ABOUT</a>
+      <a id="button-contact" href="#">CONTACT</a>
+      {/*<div id="button-get-started">
+        <div id="button-get-started-rectangle">*/}
+          <button id="button-get-started-text" type="button">GET STARTED</button>
+        {/*</div>
+      </div>*/}
+      {/*<Header />
 
         <Homepage />
 
-      <Footer />
+      <Footer />*/}
       
     </div>
   );
