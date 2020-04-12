fe6f493cf1f7badf2c29c9c1e01fb9e4a1f7d624
Elzbieta <elzbieta.glazar@gmail.com>
2020-04-12 23:53:08 +0200

    Implemening a chart on the Coordinator's Show Results Page

---
+++ frontend/survey-app/src/components/pages/CoordinatorsShowResultsPage.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import { Button } from 'reactstrap'; // { { Button, Table}
+import history from '../../history';
+import Ellipse from '../ellipseComponent/ellipse';
+import Chart from '../chartComponent/chartComponent';
+
+function CoordinatorsShowResultsPage() {
+
+  return (
+    <div id="create-survey-page-container">
+      <form>
+            <div id="input-group-create-survey-page">
+            
+              <label>Survey Name:</label>
+             
+              <input
+                type="text"
+                id="input-create-survey-page-name"
+                
+                />
+                </div>
+              <div id="input-group-create-survey-page">
+              <label>Question:</label>
+              <input
+                type="text"
+                id="input-create-survey-page-question"
+                
+                />
+                </div>
+
+              <div id="input-group-create-survey-page">
+              <label>Answers:</label>
+              <div> <ol type="A">
+              <li><input
+                type="text"
+                id="input-show-results-page-answers"
+                
+                /></li>
+                </ol>
+                </div>
+              </div>
+
+              {/*chart implementation*/}
+              <div class="chart-container">
+                <canvas id="survey-results-chart" width="800" height="450"></canvas>  
+              </div>
+
+              <Chart /*chartData={this.state.chartData}*//>
+            
+      
+      </form>
+     
+
+      <Ellipse />
+    </div>
+  );
+}
+
+export default CoordinatorsShowResultsPage;
