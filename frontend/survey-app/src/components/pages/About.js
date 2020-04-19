14b52c04476aa88f8c01acae37eade3860b8341b
Elzbieta <elzbieta.glazar@gmail.com>
2020-04-19 16:02:10 +0200

    Adding content to the About and Contact Page

--- frontend/survey-app/src/components/pages/About.js
+++ frontend/survey-app/src/components/pages/About.js
@@ -8,7 +8,16 @@ function About() {
       <p id="landing-page-logo">SurveyApp</p>
       <Nav />
       <Ellipse />
-      <h1>About Page</h1>
+      <div className="about-section">
+      
+      <div>
+        Surveys are a valuable part of any company's toolbox. They are useful for getting feedback from your customers on a variety of topics including application satisfaction, requirements, upcoming needs, issues, priorities, and just plain aggravations to name a few.
+      </div>
+      <p></p>
+      <div id="new-paragraph">
+        <span Style="color:#EB5757; font-size: 55px">Our Survey app gives you the opportunity to receive feedback from all angles. </span><br />It provides the ability to define a survey, allow users to respond within a predefined timeframe, and tablulate and present results.
+      </div>
+      </div>
 
     
     </div>
--- frontend/survey-app/src/components/pages/Contact.js
+++ frontend/survey-app/src/components/pages/Contact.js
@@ -8,7 +8,12 @@ function Contact() {
     <p id="landing-page-logo">SurveyApp</p>
       <Nav />
       <Ellipse />
-      <h1>Contact Page</h1>
+      <h1>Our Team</h1>
+      <div className="contact-page-container">
+        <p id="contact-item">Elżbieta Glazar <br /><a href="mailto: elzbieta.glazar@gmail.com">elzbieta.glazar@gmail.com</a></p>
+        <p id="contact-item">Bartosz Suchocki <br /><a href="mailto: bartosz.suchocki@gmail.com"> bartosz.suchocki@gmail.com</a></p>
+        <p id="contact-item">Andrzej Szelemetko <br /><a href="mailto: andrzej.szelemetko@gmail.com"> andrzej.szelemetko@gmail.com</a></p>
+      </div>
 
     
     </div>
